13d9257d314fe6d980dd5947badf7a0a851d1812
MarianaOsorioM <maosoriome@unal.edu.co>
2025-11-12 21:50:09 -0500

    Intento 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,6 +5,100 @@ Escriba el codigo que ejecute la accion solicitada.
 
 
 def pregunta_01():
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import os
+
+    output_dir = "files/docs"
+    os.makedirs(output_dir, exist_ok=True)
+
+    def load_data():
+        df=pd.read_csv('files/input/shipping-data.csv')
+        return df
+    
+    def create_visual_for_shipping_per_warehouse(df):
+        df=df.copy()
+        plt.figure()
+        counts=df.Warehouse_block.value_counts()
+        counts.plot.bar(
+            title='Shipping per Warehouse',
+            xlabel='Warehouse block',
+            ylabel='Record Count',
+            color='tab:blue',
+            fontsize=8,
+        )
+        plt.gca().spines['top'].set_visible(False)
+        plt.gca().spines['right'].set_visible(False)
+        
+        plt.savefig('docs/shipping_per_warehouse.png')
+        
+    def create_visual_for_mode_of_shipment(df):
+        df=df.copy()
+        plt.figure()
+        counts=df.Mode_of_Shipment.value_counts()
+        counts.plot.pie(
+            title='Mode Of Shipment',
+            wedgeprops=dict(width=0.35), 
+            ylabel='',
+            colors=['tab:blue','tab:orange','tab:green'],
+        )
+        plt.savefig('docs/mode_of_shipment.png')
+        
+    def create_visual_for_average_customer_raiting(df):
+        df=df.copy()
+        plt.figure()
+        df=(
+            df[['Mode_of_Shipment','Customer_rating']].groupby('Mode_of_Shipment').describe()
+        )
+        df.columns=df.columns.droplevel()
+        df=df[['mean','min','max']]
+        plt.barh(
+            y=df.index.values,
+            width=df['max'].values-1,
+            left=df['min'].values,
+            height=0.9,
+            color='lightgray',
+            alpha=0.8, 
+            )
+        colors=[
+            'tab:green' if value >= 3 else 'tab:orange' for value in df['mean'].values
+        ]
+        plt.barh(
+            y=df.index.values,
+            width=df['mean'].values-1,
+            left=df['min'].values,
+            color=colors,
+            height=0.5,
+            alpha=1, 
+        )
+        plt.title('Average Customer Rating')
+        plt.gca().spines['left'].set_color('gray')
+        plt.gca().spines['top'].set_visible(False)
+        plt.gca().spines['right'].set_visible(False)
+        plt.gca().spines['bottom'].set_color('gray')      
+    
+        plt.savefig('docs/average_customer_rating.png')
+    
+    def create_visual_for_weight_distribution(df):
+        df=df.copy()
+        plt.figure()
+        df.Weight_in_gms.plot.hist(
+            title='Shipped Weight Distribution',
+            color='tab:orange',
+            edgecolor='white',
+        )
+        plt.gca().spines['top'].set_visible(False)
+        plt.gca().spines['right'].set_visible(False)
+    
+        plt.savefig('docs/weight_distribution.png')
+    
+    os.makedirs('docs',exist_ok=True)
+    df=load_data()
+    create_visual_for_shipping_per_warehouse(df)
+    create_visual_for_mode_of_shipment(df)
+    create_visual_for_average_customer_raiting(df)
+    create_visual_for_weight_distribution(df)
+                
     """
     El archivo `files//shipping-data.csv` contiene información sobre los envios
     de productos de una empresa. Cree un dashboard estático en HTML que
@@ -35,3 +129,5 @@ def pregunta_01():
     * Su código debe crear la carpeta `docs` si no existe.
 
     """
+
+pregunta_01()
